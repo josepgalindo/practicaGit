8991f72669cec6332666a9c4404e558a30ccc91d
josepgalindo <jgcastar@gmail.com>
2025-12-15 16:28:10 +0100

    feat: Afegir sistema d'usuaris

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Sistema de Gestió de Tasques
+// Versió 1.0
+
+function mostrarUsuari($nom) {
+    echo "=== GESTOR DE TASQUES ===\n";
+    echo "Usuari actual: $nom\n";
+    echo "Benvingut al sistema!\n\n";
+}
+
+$usuariActual = "Josep galindo castarlenas";
+
+mostrarUsuari($usuariActual);
+?>
