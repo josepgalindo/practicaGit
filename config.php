783657401171108cfe684b3c1cabbafa1b3219f8
josepgalindo <jgcastar@gmail.com>
2025-12-15 23:34:08 +0100

    feat: Afegir configuració de base de dades

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Configuració de la base de dades
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'usuari_daw');
+define('DB_PASS', ''); // Sense contrasenya en desenvolupament local
+define('DB_NAME', 'tasques_db');
+?>
